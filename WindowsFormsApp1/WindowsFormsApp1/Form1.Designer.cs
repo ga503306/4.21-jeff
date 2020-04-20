c566e5096a4bd5c0b92eb07fa62bc9da0724b817
hzt <ga203306@yahoo.com.tw>
2020-04-21 00:10:29 +0800

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.img_path = new System.Windows.Forms.TextBox();
+            this.btn_selpath = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_change = new System.Windows.Forms.Button();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.sel_txt = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // img_path
+            // 
+            this.img_path.Location = new System.Drawing.Point(53, 50);
+            this.img_path.Name = "img_path";
+            this.img_path.Size = new System.Drawing.Size(363, 22);
+            this.img_path.TabIndex = 0;
+            // 
+            // btn_selpath
+            // 
+            this.btn_selpath.Location = new System.Drawing.Point(413, 50);
+            this.btn_selpath.Name = "btn_selpath";
+            this.btn_selpath.Size = new System.Drawing.Size(30, 23);
+            this.btn_selpath.TabIndex = 1;
+            this.btn_selpath.Text = "...";
+            this.btn_selpath.UseVisualStyleBackColor = true;
+            this.btn_selpath.Click += new System.EventHandler(this.btn_selpath_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "狀態";
+            // 
+            // btn_change
+            // 
+            this.btn_change.Location = new System.Drawing.Point(53, 119);
+            this.btn_change.Name = "btn_change";
+            this.btn_change.Size = new System.Drawing.Size(75, 23);
+            this.btn_change.TabIndex = 3;
+            this.btn_change.Text = "修改";
+            this.btn_change.UseVisualStyleBackColor = true;
+            this.btn_change.Click += new System.EventHandler(this.Btn_change_Click);
+            // 
+            // txt_path
+            // 
+            this.txt_path.Location = new System.Drawing.Point(53, 79);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.Size = new System.Drawing.Size(363, 22);
+            this.txt_path.TabIndex = 5;
+            // 
+            // sel_txt
+            // 
+            this.sel_txt.Location = new System.Drawing.Point(413, 78);
+            this.sel_txt.Name = "sel_txt";
+            this.sel_txt.Size = new System.Drawing.Size(30, 23);
+            this.sel_txt.TabIndex = 6;
+            this.sel_txt.Text = "...";
+            this.sel_txt.UseVisualStyleBackColor = true;
+            this.sel_txt.Click += new System.EventHandler(this.Sel_txt_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 223);
+            this.Controls.Add(this.sel_txt);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.btn_change);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_selpath);
+            this.Controls.Add(this.img_path);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox img_path;
+        private System.Windows.Forms.Button btn_selpath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_change;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.Button sel_txt;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<string> listStr = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Button
+        private void btn_selpath_Click(object sender, EventArgs e) //選擇圖片路徑
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            this.img_path.Text = path.SelectedPath;
+        }
+
+        private void Sel_txt_Click(object sender, EventArgs e) //選擇txt路徑
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (string.IsNullOrEmpty(ofd.InitialDirectory))
+                ofd.InitialDirectory = "D:";
+
+            ofd.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            ofd.Title = "請開啟文字檔案";
+
+            if (ofd.ShowDialog(this) == DialogResult.Cancel)
+                return;
+            txt_path.Text = ofd.FileName;
+            Read_txt();
+        }
+
+        private void Btn_change_Click(object sender, EventArgs e) //開始改檔名
+        {
+            //Read_ALLfile();
+            Change_Allfile();
+        }
+        #endregion
+
+        #region Function
+        public void Read_txt() //讀檔
+        {
+            string line;
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(txt_path.Text);
+            while ((line = file.ReadLine()) != null)
+            {
+                listStr.Add(line);
+            }
+
+            file.Close();
+        }
+
+        public void Read_ALLfile()  // 取得資料夾內所有檔案
+        {
+            foreach (string fname in System.IO.Directory.GetFiles(img_path.Text))
+            {
+                string line = "";
+                System.IO.StreamReader file = new System.IO.StreamReader(fname);
+                while ((line = file.ReadLine()) != null)
+                {
+                    label1.Text = label1.Text + line.Trim();
+                }
+
+                file.Close();
+            }
+
+        }
+        public void Change_Allfile() //修改檔案名稱
+        {
+            DirectoryInfo di = new DirectoryInfo(img_path.Text);
+            Int32 count = 0;
+            foreach (FileInfo fi in di.GetFiles("*.jpg"))
+            {
+                String NewFileName = listStr[count];
+                //重新命名
+                fi.MoveTo(Path.Combine(fi.DirectoryName, NewFileName + fi.Extension));
+                count++;
+            }
+            label1.Text = "修改完成";
+        } 
+        #endregion
+
+
+
+    }
+
+
+}
